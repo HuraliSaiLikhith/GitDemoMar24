b2b22572074f1ac7422d2ff69bee7e2c75820a01
sai likith <huralilikith97@gmail.com>
2024-03-31 09:48:24 +0530

    Added comments

--- main.java
+++ main.java
@@ -5,3 +5,15 @@ public class main {
 		System.out.println("Addition" + Calculator.add(10, 15));
 	}
 }
+/*
+	master
+	public class Main{
+		public static void main(String[] args){
+			System.out.println("Hello World");
+		}
+	}
+
+	calculator
+	14 - System.out.println("Addition" + Calculator.add(10, 15));
+	
+ */
