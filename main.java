3736fff89861a7cf6f609114648a80a74a7baeb4
sai likith <huralilikith97@gmail.com>
2024-03-31 09:03:33 +0530

    Added calculator

--- main.java
+++ main.java
@@ -1,5 +1,7 @@
-public class Main{
+public class main {
 	public static void main(String[] args){
+
 		System.out.println("Hello World");
+		System.out.println("Addition" + Calculator.add(10, 15));
 	}
 }
